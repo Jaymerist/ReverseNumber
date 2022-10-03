cd4c7c0fe616b804ddc5ed8bc2b1232d741fff14
Jaymerist <76460057+Jaymerist@users.noreply.github.com>
2022-10-03 11:27:52 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AgeForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            string number;
+            int  length;
+
+            //input number/string
+            Console.Write("Enter a number: ");
+            number = Console.ReadLine();
+            length = number.Length;
+            //loop
+            // - get length of the string
+            // - start at the end of the string and change the index to the beginning (0) in a for loop
+
+            for (int start = length - 1; start >= 0; start--)
+            {
+                Console.WriteLine(number.Substring(start, 1));
+            }  
+
+            Console.ReadLine();
+        }
+    }
+}
